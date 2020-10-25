3f274ee548caf85f61596e2e37af428d635d5d47
wukong <wukong@163.com>
2020-10-25 18:26:15 +0800

    add conf

---
+++ iweb_mini_lib/conf/dbconf.php
@@ -0,0 +1,19 @@
+<?php
+$host = "47.242.146.195";//mysql数据库服务器,比如localhost:3306
+$db = "www_partyings_co"; //默认数据库名
+$user = "www_partyings_co"; //mysql数据库默认用户名
+$pwd = "N7FZKEnGirajFZ8r"; //mysql数据库默认密码
+
+global $tablePreStr;//设置外部变量
+$tablePreStr = "wy_";//表前缀
+
+$ws_url = "wss://{$_SERVER['HTTP_HOST']}/socket";
+//当前提供服务的mysql数据库
+global $dbServs;
+$dbServs = array(
+    'host'=>$host,
+    'db'=>$db,
+    'user'=>$user,
+    'pwd'=>$pwd
+);
+?>
